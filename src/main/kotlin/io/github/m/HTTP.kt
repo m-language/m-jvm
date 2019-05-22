59099d787173446803c8c895944256e037ef1127
Kaden Thomas <thomas.kaden4@outlook.com>
2019-05-22 16:47:18 -0700

    Update gradle to 5.4.1

--- src/main/kotlin/io/github/m/HTTP.kt
+++ src/main/kotlin/io/github/m/HTTP.kt
@@ -2,12 +2,11 @@ package io.github.m
 
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Method
-import java.lang.IllegalStateException
 
 object HTTP {
     @Suppress("unused")
     object Definitions {
-        val methodMap: Map<String, Method> =
+        private val methodMap =
                 mapOf(
                         "GET" to Method.GET,
                         "HEAD" to Method.HEAD,
@@ -17,27 +16,28 @@ object HTTP {
                         "DELETE" to Method.DELETE
                 )
 
-        @MField(name="http/send")
+        @MField(name = "http/send")
         @JvmField
         val sendRequest: Value = Value.Impl4("http/send") { method, url, headers, body ->
-            val reqHeaders = List.from(headers).map { List.from(it) }.map {
-                val headerName = Symbol.from(it.first()).value
-                val values = it.drop(1).toList()
+            val reqHeaders = List.from(headers).map {
+                val list = List.from(it)
+                val headerName = Symbol.from(list.first()).value
+                val values = (list as List.Cons).cdr
                 Pair(headerName, values)
             }.toMap()
 
-            val httpMethod = method as Symbol
+            val httpMethod = Symbol.from(method)
             val methodUsed = methodMap[httpMethod.value]
-                    ?: throw IllegalStateException("$httpMethod is not a valid HTTP method")
+                    ?: return@Impl4 Error("$httpMethod is not a valid HTTP method")
 
             val request = Fuel.request(methodUsed, Symbol.from(url).value)
                     .header(reqHeaders)
 
             Process {
-                val fullRequest = when(body) {
+                val fullRequest = when (body) {
                     is File -> request.body(body.value)
                     is Symbol -> request.body(body.value)
-                    else -> throw IllegalStateException("$body is not a valid request body")
+                    else -> return@Process Error("$body is not a valid request body")
                 }
                 Symbol.valueOf(fullRequest.responseString().second.body().asString(null))
             }
